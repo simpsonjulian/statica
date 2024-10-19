c4932888b4ac1a01446565c3142b00a9e5ba5e53
Julian Simpson <julian@safeadvisory.co.nz>
2024-10-19 14:46:35 +1300

    Install bearer as dependency

--- live.sh
+++ live.sh
@@ -6,10 +6,11 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 set -euo pipefail
 
-brew install simpsonjulian/statica-tap/statica
+brew install simpsonjulian/statica-tap/statica bearer/tap/bearer
 
 pipx install sarif-tools
 npm install -g jscpd-sarif-reporter
+
 cd /tmp
 
 gh repo clone WebGoat/WebGoat
